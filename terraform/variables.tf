472636a87ca1f0b4722c1dcaf7f7d1e71609e950
makwaji <makwana.jignesh@gmail.com>
2024-10-10 12:13:08 +0800

    TF Issue fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,4 +2,5 @@ variable "sso_principal_arn" {
   description = "SSO Arn."
   type        = string
   sensitive   = true
+  default     = "arn:aws:iam::851725219470:role/aws-reserved/sso.amazonaws.com/eu-central-1/AWSReservedSSO_AdnAccountFullAccess_94a9e9fa13eb7e4f"
 }
