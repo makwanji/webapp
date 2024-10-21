72a7355796d2d1aee4443a804651b3aadf847a43
makwaji <makwana.jignesh@gmail.com>
2024-10-21 17:29:23 +0800

    backend code change

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,18 +3,17 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: 'postgres',
-  });
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,     // Database name
+  process.env.DB_USER,     // Database username
+  process.env.DB_PASSWORD,     // Database password
+  {
+    host: process.env.DB_HOST,   // Database host
+    dialect: process.env.DB_DIALECT || 'postgres',  // Default to PostgreSQL
+  }
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
--- deploy/README.md
+++ deploy/README.md
@@ -1,1 +1,14 @@
 # Deploy application
+
+
+kubectl create secret docker-registry regcred \
+  --docker-server=869388145954.dkr.ecr.ap-southeast-1.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password) \
+  -n ps
+
+
+
+docker tag webapp-postgres:v0.0.1 makwanji/webapp-postgres:v0.0.1
+docker tag webapp-back:v0.0.4 makwanji/webapp-back:v0.0.4
+docker tag webapp-front:v0.0.7 makwanji/webapp-front:v0.0.7
